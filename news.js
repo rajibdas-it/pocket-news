7c3332ec56cb6ba0caf6714b884adf248c04bfad
rajibdas-it <rajibrad@gmail.com>
2022-09-03 01:38:54 +0600

    category wise data fetches..

--- news.js
+++ news.js
@@ -12,14 +12,19 @@ const displayCategoryName = (categories) => {
     categoryDiv = document.createElement("div");
     categoryDiv.classList.add("col");
     categoryDiv.innerHTML = `
-    <button class="btn p-3" onclick="loadNews('${category.category_id}')">${category.category_name}</button>
+    <button class="btn p-2 mb-3" onclick="loadNews('${category.category_id}')">${category.category_name}</button>
     `;
     categoriesSection.appendChild(categoryDiv);
   });
 };
 
-const loadNews = (categoryId) => {
-  console.log(categoryId);
+const loadNews = (news_id) => {
+  console.log(news_id);
+  const url = `https://openapi.programming-hero.com/api/news/category/${news_id}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => console.log(data.data));
+  // '${category.category_id}'
 };
 
 loadCategory();
