d04af164fc039575355d3db33fca31320772e4f4
rajibdas-it <rajibrad@gmail.com>
2022-09-03 01:04:17 +0600

    view category name dynamically

--- news.js
+++ news.js
@@ -1,1 +1,21 @@
-console.log("Connected");
+const loadCategory = () => {
+  const url = "https://openapi.programming-hero.com/api/news/categories";
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayCategoryName(data.data.news_category));
+};
+
+const displayCategoryName = (categories) => {
+  console.log(categories);
+  const categoriesSection = document.getElementById("categories-section");
+  categories.forEach((category) => {
+    categoryDiv = document.createElement("div");
+    categoryDiv.classList.add("col");
+    categoryDiv.innerHTML = `
+    <a href="" class="btn btn-light">${category.category_name}</a>
+    `;
+    categoriesSection.appendChild(categoryDiv);
+  });
+};
+
+loadCategory();
