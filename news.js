80ddf15332193962e60163b78a1833caab499dd8
Rajib Das <rajibrad@gmail.com>
2022-09-03 13:41:28 +0600

    modal section complete

--- news.js
+++ news.js
@@ -19,7 +19,7 @@ const displayCategoryName = (categories) => {
 };
 
 const loadNews = (news_id) => {
-  console.log(news_id);
+  //console.log(news_id);
   const url = `https://openapi.programming-hero.com/api/news/category/${news_id}`;
   fetch(url)
     .then((res) => res.json())
@@ -36,6 +36,120 @@ const displayNews = (items) => {
     alertMsg.classList.remove("d-none");
     alertMsg.innerText = `No items found`;
   }
+
+  const newsSection = document.getElementById("news-section");
+  newsSection.textContent = "";
+  items.forEach((news) => {
+    const newsDiv = document.createElement("div");
+    newsDiv.innerHTML = `
+    <div class="mx-auto card mb-3">
+    <div class="row g-0">
+      <div class="col-md-4">
+        <img
+          src=${news.thumbnail_url ? news.thumbnail_url : "No image found!"}
+          class="img-fluid h-100 w-100 rounded-start"
+          alt="..."
+        />
+      </div>
+      <div class="col-md-8">
+        <div class="card-body">
+          <h5 class="card-title">${news.title}</h5>
+          <p class="card-text">${
+            news.details.length > 1500
+              ? news.details.slice(0, 1500) + "..."
+              : news.details
+          }</p>
+          <p class="card-text">
+            <small class="text-muted">Published on: ${
+              news.author.published_date
+                ? news.author.published_date
+                : "data not found"
+            }</small>
+          </p>
+        </div>
+        <div class="mb-1 d-flex gap-1 position-bottom-0">
+          <div class="ms-3 d-flex flex-column flex-md-row align-items-center justify-content-center">
+            <img
+              style="width: 50px; height: 50px"
+              src=${news.author.img ? news.author.img : "no data found"}
+              class="img-fluid rounded-circle p-2"
+              alt=""
+            />
+            <h3>${news.author.name ? news.author.name : "no data found"}</h3>
+          </div>
+          <div class="ms-3">
+            <p><i class="fa-solid fa-eye"></i>${
+              news.total_view ? news.total_view : "no data found"
+            }</p>
+          </div>
+          <div class="ms-3">
+            <p>rating ${
+              news.rating.number ? news.rating.number : "no data found"
+            }</p>
+          </div>
+          <div class="ms-3 me-3">
+          <button onclick="loadDetailsNews('${
+            news._id
+          }')"  type="button" class="btn text-info fw-bold fs-4 me-5" data-bs-toggle="modal" data-bs-target="#exampleModal">
+          <i class="fa-solid fa-arrow-right"></i>
+          </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  
+    `;
+    newsSection.appendChild(newsDiv);
+  });
 };
 
+const loadDetailsNews = (news_id) => {
+  const url = `https://openapi.programming-hero.com/api/news/${news_id}`;
+  // console.log(url);
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayDetailsNews(data.data[0]));
+};
+
+const displayDetailsNews = (news) => {
+  console.log(news);
+  const modalTitle = document.getElementById("modal-title");
+  modalTitle.innerText = news.title;
+  const modalBody = document.getElementById("modal-body");
+  // const modalFooter = document.getElementById("modal-footer");
+  modalBody.innerHTML = `
+        <div class="row">
+          <div class="col-10 mx-auto w-100">
+            <img src=${
+              news.image_url ? news.image_url : "No Image"
+            } class="img-fluid rounded" alt="" />
+          </div>
+
+          <h1 class="mt-3">${news.title}</h1>
+          <div class="d-flex flex-row">
+           <div>
+           <img
+           style="width: 50px; height: 50px"
+           src=${news.author.img ? news.author.img : "no data found"}
+           class="img-fluid rounded-circle p-2"
+           alt=""
+         />
+           </div>
+            <div>
+            <p class="mt-3"><small>${
+              news.author.name ? news.author.name : "no data found"
+            }</small>, <small>published on: </small>${
+    news.author.published_date ? news.author.published_date : "no data found"
+  }</small></p></div>
+          </div>
+          
+          <p class="mt-3">${news.details}</p>
+     
+        </div>
+  
+  `;
+};
+
+loadNews("04");
 loadCategory();
---
+++ test.js
@@ -0,0 +1,11 @@
+const details = `1,000 pairs of autographed Size 22 Reeboks were made available thanks to a partnership between Shaquille 
+O’Neal and Steiner Sports. Shaquille O’Neal is a larger-than-life character in every way. We’re talking about 
+a superstar who earned as many self-styled nicknames as he did awards, a man who made his NBA debut by traveling 
+from coast to coast like a speeding freight train. Naturally, O’Neal was also physically larger than most people 
+due to his 7ft height and 300-pound weight. His absurd size 22 shoes could serve as an oversized phone for Allen I
+verson. .@SHAQ‘s signature is going global. The 4x @NBA champ is bringing his SHAQ shoes to South Africa — priced 
+between R799 & R999 ($52 – $65 USD) for children and adults. Shaq says the current line is aimed at “everyday 
+consumers.” pic.twitter.com/MWEHdVZOX0 — Boardroom (@boardroom) January 16, 2022 O’Neal is a well-known figure 
+outside of`;
+
+console.log(details.length);
