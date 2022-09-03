3a6f371949749aea02dfdb0cb923439e82d762a2
Rajib Das <rajibrad@gmail.com>
2022-09-03 10:04:02 +0600

    added alert msg

--- news.js
+++ news.js
@@ -12,7 +12,7 @@ const displayCategoryName = (categories) => {
     categoryDiv = document.createElement("div");
     categoryDiv.classList.add("col");
     categoryDiv.innerHTML = `
-    <button class="btn p-2 mb-3" onclick="loadNews('${category.category_id}')">${category.category_name}</button>
+    <button class="btn p-2 mb-3 fw-bold" onclick="loadNews('${category.category_id}')">${category.category_name}</button>
     `;
     categoriesSection.appendChild(categoryDiv);
   });
@@ -23,8 +23,20 @@ const loadNews = (news_id) => {
   const url = `https://openapi.programming-hero.com/api/news/category/${news_id}`;
   fetch(url)
     .then((res) => res.json())
-    .then((data) => console.log(data.data));
-  // '${category.category_id}'
+    .then((data) => displayNews(data.data));
+};
+
+const displayNews = (items) => {
+  console.log(items);
+  const alertMsg = document.getElementById("alert-msg");
+  if (items.length > 0) {
+    alertMsg.classList.remove("d-none");
+    alertMsg.innerText = `${items.length} items found`;
+  } else {
+    alertMsg.classList.remove("d-none");
+
+    alertMsg.innerText = `No items found`;
+  }
 };
 
 loadCategory();
