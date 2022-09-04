17be3c104b791399334ff87c8fa554c7e168d44b
Rajib Das <rajibrad@gmail.com>
2022-09-04 09:24:00 +0600

    set a function for default news

--- news.js
+++ news.js
@@ -19,6 +19,110 @@ const displayCategory = (categories) => {
   });
 };
 
+//Default news show when home page loading
+
+const loadDefaultNews = () => {
+  const url = `https://openapi.programming-hero.com/api/news/category/08`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayDefaultNews(data.data))
+    .catch((err) => console.log(err));
+  toggleSpinner(true);
+};
+
+const displayDefaultNews = (items) => {
+  //news item sorted accroding to the highest view.
+  const sortedNews = items.sort((a, b) => {
+    return b.total_view - a.total_view;
+  });
+  const newsSection = document.getElementById("news-section");
+  newsSection.textContent = "";
+  sortedNews.forEach((news) => {
+    //object destructuring
+    const { _id, rating, total_view, title, author, thumbnail_url, details } =
+      news;
+    const { number, badge } = rating;
+    const { name, published_date, img } = author;
+
+    const newsDiv = document.createElement("div");
+    newsDiv.innerHTML = `
+    <div class="card mb-3">
+        <div class="row g-0">
+          <div class="col-md-4">
+            <img
+              style="height: 300px"
+              src=${thumbnail_url ? thumbnail_url : "no data found"}
+              class="img-fluid rounded-start w-100 "
+              alt="..."
+            />
+          </div>
+          <div class="col-md-8 d-flex flex-column justify-content-between">
+            <div class="card-body">
+              <h5 class="card-title">${title ? title : "no data found"}</h5>
+              <p class="card-text">
+                <small class="text-muted">Published on: ${
+                  published_date ? published_date : "no data found"
+                }</small>
+              </p>
+              <p class="card-text">
+              ${
+                details.length > 250
+                  ? details.slice(0, 250) + "..."
+                  : news.details
+              }
+              </p>
+            </div>
+            <div class="card-footer d-flex justify-content-between flex-row">
+              <div class="d-flex justify-content-around flex-row">
+                <img
+                  style="width: 50px; height: 50px"
+                  class="rounded-circle img-fluid img-thumbnail"
+                  src=${img ? img : "no data found"}
+                  alt=""
+                />
+                <p class="ms-2">${name ? name : "no data found"}</p>
+              </div>
+              <div class="total-view">
+                <p><i class="fa-solid fa-eye"></i>${
+                  total_view ? total_view : "no data found"
+                }</p>
+              </div>
+              <div class="total-rating">
+                <p>
+                <span style="color: orange;" class="fa fa-star"></span>
+                <span style="color: orange;" class="fa fa-star"></span>
+                <span style="color: orange;" class="fa fa-star"></span>
+                <span style="color: orange;" class="fa fa-star"></span>
+                <span class="fa fa-star"></span> ${
+                  number ? number : "no data found"
+                }
+                </p>
+               
+              </div>
+              <div class="read-more">
+                <button
+                  onclick="loadDetailsNews('${_id}')"
+                  type="button"
+                  class="btn text-info fw-bold fs-3"
+                  data-bs-toggle="modal"
+                  data-bs-target="#exampleModal"
+                >
+                  <i class="fa-solid fa-arrow-right"></i>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    
+  
+    `;
+    newsSection.appendChild(newsDiv);
+  });
+  toggleSpinner(false);
+};
+
+// news show click on category button
 const loadNews = (news_id) => {
   const url = `https://openapi.programming-hero.com/api/news/category/${news_id}`;
   fetch(url)
@@ -28,7 +132,7 @@ const loadNews = (news_id) => {
     .then((data) =>
       setTimeout(() => {
         displayNews(data.data);
-      }, 1000)
+      }, 500)
     )
     .catch((err) => console.log(err));
   toggleSpinner(true);
@@ -208,5 +312,6 @@ const toggleSpinner = (isLoading) => {
 //   }
 // };
 
-loadNews("08");
+// loadNews("08");
+loadDefaultNews();
 loadCategory();
